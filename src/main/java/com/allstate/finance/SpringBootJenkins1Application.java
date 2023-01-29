344ada34bb312baf20c736c529cb542e8ee90357
manash <ranjanmanash275@gmail.com>
2023-01-29 17:42:04 +0530

    first commit

---
+++ src/main/java/com/allstate/finance/SpringBootJenkins1Application.java
@@ -0,0 +1,14 @@
+package com.allstate.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkins1Application.class, args);
+		System.out.println("Omm Sai Ram");
+	}
+
+}
---
+++ src/test/java/com/allstate/finance/SpringBootJenkins1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkins1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
